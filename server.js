3122c4dea3c7a749a365ea42ccd8c59e93a6656e
mushtaq <mushtaq.safie@students.mq.edu.au>
2021-05-06 19:48:52 +1000

    setup server

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const path = require("path");
+const mysql2 = require("mysql2");
+
+//// const routes = require("./routes");
+
+// Requiring our models for syncing
+//// const db = require("./models");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+// Add routes, both API and view
+//// app.use(routes);
+
+// Send every other request to the React app
+// Define any API routes before this runs
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+
+// Syncing our sequelize models and then starting our Express app
+// !! REMOVE "{ force: true }" @ deployment !!
+db.sequelize.sync({ force: true }).then(() => {
+  // Start the API server
+  app.listen(PORT, function() {
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+  });
+});
+
