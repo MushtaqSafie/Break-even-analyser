2fa9a4d7989a8398084e68eb882174f64ea49116
mushtaq <mushtaq.safie@students.mq.edu.au>
2021-05-07 18:53:26 +1000

    setup react-router-dom for app routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import { StoreProvider } from "./utils/GlobalState";
+import Home from "./pages/Home"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
+import NoMatch from "./pages/NoMatch"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        {/* <StoreProvider> */}
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/signin" component={SignIn} />
+            <Route exact path="/signup" component={SignUp} />
+            <Route component={NoMatch} />
+          </Switch>
+        {/* </StoreProvider> */}
+      </div>
+    </Router>
   );
 }
 
