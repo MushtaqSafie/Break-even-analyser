0aba9044451c754fb587f69e0c1a9782fc2a00ba
mushtaq <mushtaq.safie@students.mq.edu.au>
2021-05-14 23:21:06 +1000

    define React Route for authenticated user

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,13 +1,17 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Redirect } from 'react-router'
+import { useStoreContext } from "./GlobalState";
 
 
-const AuthRoute = ({ component: Component, ...rest }) => (
-  <Route {...rest} render={(props) => (
-    true
-      ? <Component {...props} />
-      : <Redirect to='/signin' />
-  )} />
-)
+const AuthRoute = ({ component: Component, ...rest }) => {
+  const [state, dispatch] = useStoreContext();
+  return (
+    <Route {...rest} render={(props) => (
+      state.isAuthenticated
+        ? <Component {...props} />
+        : <Redirect to='/signin' />
+    )} />
+  )  
+}
 
 export default AuthRoute;
