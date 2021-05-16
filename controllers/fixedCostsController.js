ab3eb2a7250e56c2dae9baf2bdbb257b3d835dfd
mushtaq <mushtaq.safie@students.mq.edu.au>
2021-05-16 18:03:37 +1000

    database CRUD operations

--- controllers/fixedCostsController.js
+++ controllers/fixedCostsController.js
@@ -1,27 +1,23 @@
 const db = require('../models');
 
 module.exports ={
-  // get all items from ProductInfo 
   findAll: function (req, res) {
-    db.Products.findAll()
+    db.FixedCosts.findAll()
     .then(dbModel => res.json(dbModel))
     .catch(err => res.status(422).json(err));
   },
-  // create one in ProductInfo
   createOne: function (req, res) {
-    db.Products.create(req.body)
+    db.FixedCosts.create(req.body)
     .then(dbModel => res.json(dbModel))
     .catch(err => res.status(422).json(err));
   },
-  // update one in ProductInfo
   removeOne: function(req, res) {
-    db.Products.destroy({ where: { id: req.params.id } })
+    db.FixedCosts.destroy({ where: { id: req.params.id } })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
-  // delete many selected from ProductInfo
   updateOne: function(req, res) {
-    db.Products.update(req.body , { where: { id: req.params.id } })
+    db.FixedCosts.update(req.body , { where: { id: req.params.id } })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   }
--- controllers/materialsCostsController.js
+++ controllers/materialsCostsController.js
@@ -1,27 +1,23 @@
 const db = require('../models');
 
 module.exports ={
-  // get all items from ProductInfo 
   findAll: function (req, res) {
-    db.Products.findAll()
+    db.MaterialsCosts.findAll()
     .then(dbModel => res.json(dbModel))
     .catch(err => res.status(422).json(err));
   },
-  // create one in ProductInfo
   createOne: function (req, res) {
-    db.Products.create(req.body)
+    db.MaterialsCosts.create(req.body)
     .then(dbModel => res.json(dbModel))
     .catch(err => res.status(422).json(err));
   },
-  // update one in ProductInfo
   removeOne: function(req, res) {
-    db.Products.destroy({ where: { id: req.params.id } })
+    db.MaterialsCosts.destroy({ where: { id: req.params.id } })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
-  // delete many selected from ProductInfo
   updateOne: function(req, res) {
-    db.Products.update(req.body , { where: { id: req.params.id } })
+    db.MaterialsCosts.update(req.body , { where: { id: req.params.id } })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   }
